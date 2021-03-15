8a634812d8a5d3a70340927d5c29d3eb92be3ca2
SHREYASH <shreyashadlinge@gmail.com>
2021-03-15 15:35:49 +0530

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
 
 const app = express()
+const port = process.env.PORT || 3000
+
 const publicDirectory = path.join(__dirname, "../public")
 const viewPath = path.join(__dirname, "../templates/views")
 const partialsPath = path.join(__dirname, "../templates/partials")
@@ -80,6 +82,6 @@ app.get("*", (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Serving on 3000")
+app.listen(port, () => {
+    console.log("Serving on " + port)
 })
